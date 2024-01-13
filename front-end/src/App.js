27a3822159e42948ae027827784aae0b1d7444e3
melatteshome <melatteshome9@gmail.com>
2024-01-13 00:57:23 +0000

    modified app.js to be routing initially to the login apge

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/login.js';
+import { AdminDashbOard} from './components/admin/admin_dashboard.js';
+import { UserPage } from './components/user/user_page.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/user" element={<UserPage />} />
+        <Route path="/admin" element={<AdminDashbOard />} />
+
+
+       
+      </Routes>
+    </Router>
   );
 }
 
