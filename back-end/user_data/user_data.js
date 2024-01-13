e2821f178f4cce54aa266b48a49eb8a888044744
melatteshome <melatteshome9@gmail.com>
2024-01-13 00:53:19 +0000

    added dummy user data for test purpuses with different roles

---
+++ back-end/user_data/user_data.js
@@ -0,0 +1,30 @@
+const users = [
+  {
+    "_id": "5c8a1d5b0190b214360dc057",
+    "name": "10 Academy",
+    "email": "10aca@gmail.com",
+    "role": "admin",
+    "active": true,
+    "password": "123456"
+  },
+  {
+    "_id": "5c8a1dfa2f8fb814b56fa181",
+    "name": "Melat Teshome",
+    "email": "melatteshome9@gmail.com",
+    "role": "user",
+    "active": true,
+    "photo": "user-2.jpg",
+    "password": "1234567"
+  },
+  {
+    "_id": "5c8a1e1a2f8fb814b56fa182",
+    "name": "Yohanes Teshome",
+    "email": "jo@gmail.com",
+    "role": "user",
+    "active": true,
+    "photo": "user-3.jpg",
+    "password": "123456"
+  }
+]
+
+module.exports = users;
