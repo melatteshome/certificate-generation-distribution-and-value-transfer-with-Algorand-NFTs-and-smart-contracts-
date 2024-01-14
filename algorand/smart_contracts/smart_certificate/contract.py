26e690fe32e08c5e2f1a5a9ab9f233624f11276d
melatteshome <melatteshome9@gmail.com>
2024-01-14 04:52:16 +0000

    algorand contact

--- algorand/smart_contracts/smart_certificate/contract.py
+++ algorand/smart_contracts/smart_certificate/contract.py
@@ -1,10 +1,21 @@
-import beaker
+import beaker as bk
 import pyteal as pt
 
 
-app = beaker.Application("smart_certificate")
+app = bk.Application("certificate")
+
 
 
 @app.external
 def hello(name: pt.abi.String, *, output: pt.abi.String) -> pt.Expr:
     return output.set(pt.Concat(pt.Bytes("Hello, "), name.get()))
+
+
+
+
+
+
+
+if __name__=="__main__":
+    spec = app.build()
+    spec.export('artifacts')
